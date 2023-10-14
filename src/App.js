ae84df4968cfd5c8a9ac6a5aa5a05203370f9ede
Karthik2154 <karthik.v2021@vitstudent.ac.in>
2023-10-14 21:58:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Task4 from "./Components/Task4";
+import "./bootstrap-5.0.2-dist/css/bootstrap.min.css";
+import "./Components/task4.css";
+
+function App() {
+  return <Task4 />;
+}
+
+export default App;
